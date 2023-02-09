112347c7dacc546904ab4f67f090def436693463
Sage Turner <sage@lamproslabs.com>
2023-02-09 11:47:47 -0500

    #18, adding CSV to JSON conversion and test file

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const csv = require("csvtojson");
 const app = express();
 const { search, ingest } = require("./services/data-access");
 const port = 3000;
@@ -29,21 +30,49 @@ app.post("/uli-service/v1/ingest/:providerUoi", async (req, res) => {
       });
     }
 
-    if (!data?.length) {
+    if (req.is('text/csv')) {
+      let csvData = ''
+      req.on('data', (chunk) => {
+        csvData += chunk.toString()
+        // Convert a csv file with csvtojson
+      });
+
+      req.on('end', async () => {
+        const jsonArray = await csv().fromString(csvData);
+        if (!jsonArray?.length) {
+          res.send({
+            statusCode: 400,
+            message: "Request body missing ULI array!",
+          });
+        }
+  
+        await ingest(providerUoi, jsonArray);
+
+        res.send({
+          statusCode: 200,
+          body: {
+            itemsProcessed: jsonArray?.length || 0
+          }
+        });
+      })
+      
+    } else if (req.is('application/json')) {
+      if (!data?.length) {
+        res.send({
+          statusCode: 400,
+          message: "Request body missing ULI array!",
+        });
+      }
+
+      await ingest(providerUoi, data);
+
       res.send({
-        statusCode: 400,
-        message: "Request body missing ULI array!",
+        statusCode: 200,
+        body: {
+          itemsProcessed: data?.length || 0
+        }
       });
     }
-
-    await ingest(providerUoi, data);
-
-    res.send({
-      statusCode: 200,
-      body: {
-        itemsProcessed: data?.length || 0
-      }
-    });
   } catch (err) {
     console.log(err);
     res.send({
